ef1e75e57c6fa36d036dcfa6b9b3ee06a20c9336
kirpitcheq <kirpitcheq@yandex.ru>
2023-06-11 22:42:22 +0300

    +timer

--- 04-delegates-events/DelegatesEvents.FileSystemWatcher/FileSystemWatcher.cs
+++ 04-delegates-events/DelegatesEvents.FileSystemWatcher/FileSystemWatcher.cs
@@ -1,14 +1,45 @@
-﻿namespace DelegatesEvents;
+﻿using System.Timers;
+namespace DelegatesEvents;
 
 public class FileSystemWatcher
 {
-    enum ChangesTypeDef {ADDED, CHANGED, DELETED}
-    class Notify
+    private readonly System.Timers.Timer timer = new System.Timers.Timer();
+    public enum ChangesTypeDef {ADDED, CHANGED, DELETED}
+    public class Notify
     {
         public ChangesTypeDef TypeOfChange { get; set; }
         public string RelativePath { get; set; } = "";
+        DateTime TimeOfChanges { get; set; }
     }
 
-    private delegate List<Notify> FilesChanged();
+    public delegate List<Notify> FilesChanged();
     public event FilesChanged? Change;
+
+    
+    //constr
+    public FileSystemWatcher(double CheckedInterval)
+    {
+        if(CheckedInterval < 0)
+            CheckedInterval = Math.Abs(CheckedInterval);
+        timer.Interval = CheckedInterval;
+        timer.Stop();
+    }
+
+    private const double default_interval = 1000.0;
+    public FileSystemWatcher() : this(default_interval) { }
+
+    public bool Subscribe (FilesChanged func) {
+        if(func == null) return false;
+        Change += func;
+        if(!timer.Enabled)
+            timer.Start();
+        return true;
+    }
+
+    public void Unsubscribe(FilesChanged func)
+    {
+        if( Change != null ) {
+        }
+    }
+
 }
