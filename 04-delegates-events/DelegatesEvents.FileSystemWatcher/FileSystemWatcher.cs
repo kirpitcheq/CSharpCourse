f6e07b6a2cd87ea3a26761581009f45ab9682e78
kpeq <kirpitcheq@yandex.ru>
2023-06-11 16:54:49 +0300

    init 04:+FileSystemWatcher

---
+++ 04-delegates-events/DelegatesEvents.FileSystemWatcher/FileSystemWatcher.cs
@@ -0,0 +1,14 @@
+﻿namespace DelegatesEvents;
+
+public class FileSystemWatcher
+{
+    enum ChangesTypeDef {ADDED, CHANGED, DELETED}
+    class Notify
+    {
+        public ChangesTypeDef TypeOfChange { get; set; }
+        public string RelativePath { get; set; } = "";
+    }
+
+    private delegate List<Notify> FilesChanged();
+    public event FilesChanged? Change;
+}
