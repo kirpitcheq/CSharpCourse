0f54c701d6f53d8e3c376bf5012afe22d2c869d2
kirpitcheq <kirpitcheq@yandex.ru>
2023-06-12 15:04:39 +0300

    .

--- 04-delegates-events/DelegatesEvents.FileSystemWatcher/FileSystemWatcher.cs
+++ 04-delegates-events/DelegatesEvents.FileSystemWatcher/FileSystemWatcher.cs
@@ -1,9 +1,12 @@
-﻿using System.Timers;
+﻿using System;
+using System.Threading;
+using System.IO;
 namespace DelegatesEvents;
 
 public class FileSystemWatcher
 {
     private readonly System.Timers.Timer timer = new System.Timers.Timer();
+    // private readonly Timer timer = new Timer(TimerWent);
     public enum ChangesTypeDef {ADDED, CHANGED, DELETED}
     public class Notify
     {
@@ -11,6 +14,8 @@ public class FileSystemWatcher
         public string RelativePath { get; set; } = "";
         DateTime TimeOfChanges { get; set; }
     }
+    private string[] files { get; set; }
+    private string path { get; set; } = "."; //this folder by default
 
     public delegate List<Notify> FilesChanged();
     public event FilesChanged? Change;
@@ -23,6 +28,24 @@ public class FileSystemWatcher
             CheckedInterval = Math.Abs(CheckedInterval);
         timer.Interval = CheckedInterval;
         timer.Stop();
+        timer.Elapsed += TimerWent!;
+    }
+    private void TimerWent(object sender, EventArgs args) {
+        var files_now = System.IO.Directory.GetFiles(path);
+        if(files_now == null)
+        {
+            if(files != null)
+                
+                //event all files are deleted (call changed event)
+            
+        }
+            return;
+        //add, del, change
+        foreach(var file in files_now) {
+            var fileinfo = new FileInfo(file);
+            var filesize = fileinfo.Length;
+            var filecreation = fileinfo.CreationTime;
+        }
     }
 
     private const double default_interval = 1000.0;
@@ -38,8 +61,7 @@ public class FileSystemWatcher
 
     public void Unsubscribe(FilesChanged func)
     {
-        if( Change != null ) {
-        }
+        
     }
 
 }
