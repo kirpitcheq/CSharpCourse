9635b8f2c3db53e681969be8393ded814f0b2e32
kirpitcheq <kirpitcheq@yandex.ru>
2023-06-12 21:59:08 +0300

    Example init

--- 04-delegates-events/DelegatesEvents.FileSystemWatcher.Example/Program.cs
+++ 04-delegates-events/DelegatesEvents.FileSystemWatcher.Example/Program.cs
@@ -1,22 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using DelegatesEvents;
+using System.IO;
+using System.Threading;
 
-const string path = ".";
+const string path = "~/tests/";
+if(!Directory.Exists(path))
+    Directory.CreateDirectory(path);
 
 DelegatesEvents.FileSystemWatcher fileSystemWatcher = new DelegatesEvents.FileSystemWatcher();
-// FilesChanged fileSystemWatcher;
 
 DelegatesEvents.FilesChanged filesChanged;
 filesChanged = ConvertAndShowMessage;
 fileSystemWatcher.Subscribe(filesChanged);
 
-Console.ReadLine();
+Thread.Sleep(2353);
+Directory.CreateDirectory(path+"test1dir");
+var file = File.Open(path+"test1dir/file.txt", FileMode.Create);
+using var streamwriter = new
 
+Thread.Sleep(1234);
 
+//----------------------------------------------------------------------------------------
 static void ConvertAndShowMessage(List<DelegatesEvents.FileSystemWatcher.Notify> notifies){
-// static void ConvertAndShowMessage(List<DelegatesEvents.FileSystemWatcher.Notify> notifies){
     if (notifies!=null)
         foreach (var notify in notifies)
             System.Console.WriteLine(notify);
